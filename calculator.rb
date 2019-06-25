453d8491e755623504f534dfdb5b2a420a862651
Eliza Munroe <era3munroe@gmail.com>
2019-06-25 12:31:47 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,6 @@
-first_number = 3
-second_number = 5
+first_number = 8
+second_number = 4
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
+quotient = first_number / second_number
